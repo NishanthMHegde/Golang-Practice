609474e752463e3f56fe116b828211049a4821cd
nishanthmhegde <=>
2019-12-12 21:36:06 +0530

    Added Hello World

---
+++ HelloWorld/main.go
@@ -0,0 +1,7 @@
+package main //indicates the name of the executable package. Executable package should always have package main
+import "fmt" //fmt is used to print statements to console and is a standard package
+
+//name of the function which will be called
+func main() {
+	fmt.Println("Hello World!!")
+}
